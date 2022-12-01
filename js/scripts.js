631dbde98214f7ae2c27e7a499b31efa724fc6fe
kaynan31 <kaynan.dev@gmail.com>
2022-12-01 10:43:13 -0300

    dark mode

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
 const changeThemeBtn = document.querySelector("#change-theme");
 
-// Toggle dark mode
 function toggleDarkMode() {
   document.body.classList.toggle("dark");
 }
 
-// Load light or dark mode
 function loadTheme() {
   const darkMode = localStorage.getItem("dark");
 
@@ -19,7 +17,6 @@ loadTheme();
 changeThemeBtn.addEventListener("change", function () {
   toggleDarkMode();
 
-  // Save or remove dark mode from localStorage
   localStorage.removeItem("dark");
 
   if (document.body.classList.contains("dark")) {
